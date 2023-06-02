413ad911a2ee25cf1eb64ffce1444cc54488c493
Frank Chambergo <franky4908@gmail.com>
2023-06-02 00:49:33 -0400

    coaching session

---
+++ coaching/index.js
@@ -0,0 +1,56 @@
+console.log("Hello Class");
+
+let x = 5;
+console.log(x)
+x = 8;
+
+console.log(x)
+
+const name = "Frank";
+console.log("My name is", name);
+console.log("My name is " + name);
+console.log('My name is ' + name);
+console.log(`My name is ${name}`)
+
+const lastName = "Chambergo";
+const streetHomeAddress = "hdshds";
+console.log(streetHomeAddress)
+
+let valid;
+console.log(valid);
+
+const num = 1;
+console.log("num",typeof num);
+
+const str = "1";
+console.log("str",typeof str);
+
+const bool = true;
+console.log("bool",typeof bool);
+
+const y = null;
+console.log("y",typeof y);
+
+const arr = ["Apple", "Banana", "Orange"];
+console.log("arr",typeof arr);
+
+const obj = {
+    firstName: "John", 
+    lastName: "Smith",
+    address: "gmass"
+};
+console.log("obj",typeof obj);
+
+let newNumber = 5;
+newNumber = newNumber + 7;
+// newNumber += 7;
+console.log(newNumber);
+
+console.log(1 == "1");
+console.log(1 === "1");
+console.log(7 > 7)
+console.log(!(10 == 10))
+
+console.log(true && false) //false
+console.log(true || false) //true
+console.log(!true) //false
